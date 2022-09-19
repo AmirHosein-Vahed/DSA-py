8fecac24d9818994d072ce7f8dc845749335a12e
AmirHosein-Vahed <vahed.ahv@gmail.com>
2022-09-19 22:08:16 +0430

    linked list completed and queue, stack folder created

--- Data structures/linkedlist/LinkedList.py
+++ Data structures/linkedlist/LinkedList.py
@@ -1,3 +1,5 @@
+import copy
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -11,6 +13,7 @@ class LinkedList:
     def __init__(self):
         self.first = None
         self.last = None
+        self.__size = 0
 
     def is_empty(self) -> bool:
         return self.first == None
@@ -23,6 +26,8 @@ class LinkedList:
             node.next = self.first
             self.first = node
 
+        self.__size += 1
+
     def add_last(self, value) -> None:
         node = Node(value)
         if self.is_empty():
@@ -30,35 +35,43 @@ class LinkedList:
         else:
             self.last.next = node
             self.last = node
+        
+        self.__size += 1
 
     def delete_first(self) -> None:
-        node = self.first
-        self.first = self.first.next
-        node.next = None
+        if self.is_empty():
+            raise Exception("linkedlist is empty")
+
+        if self.first == self.last:
+            self.first = self.last = None
+        else:
+            second = self.first.next
+            self.first.next = None
+            self.first = second
+
+        self.__size -= 1
 
     def delete_last(self) -> None:
-        current = self.first
-        while(True):
-            if current.next == self.last:
-                current.next = None
-                self.last = current
-                return
+        if self.is_empty():
+            raise Exception("linkedlist is empty")
 
-            current = current.next
-            
+        if self.first == self.last:
+            self.first = self.last = None
+        else:
+            current = self.first
+            while(True):
+                if current.next == self.last:
+                    current.next = None
+                    self.last = current
+                    break
+
+                current = current.next
+        
+        self.__size -= 1
 
     def contains(self, value) -> bool:
-        current = self.first
-        while(True):
-            if current.value == value:
-                return True
+        return self.index_of(value) != -1
             
-            if current.next == None:
-                return False
-            
-            current = current.next
-            
-
     def index_of(self, value):
         current = self.first
         index = 0
@@ -71,3 +84,63 @@ class LinkedList:
 
             current = current.next
             index += 1
+
+    def size(self):
+        return self.__size
+
+    def to_array(self):
+        if self.is_empty():
+            return []
+
+        arr = []
+        current = self.first
+        while(True):
+            arr.append(current.value)
+
+            if current.next == None:
+                return arr
+
+            current = current.next
+
+    def reverse(self):
+        if self.is_empty():
+            return
+        if self.first == self.last:
+            return
+
+        prev = self.first
+        curr = self.first.next
+        nex  = self.first.next.next
+    
+        while(True):
+            if curr == None:
+                self.last = self.first
+                self.last.next = None
+                self.first = prev
+                return
+
+            curr.next = prev
+
+            prev = curr
+            curr = nex
+            if nex != None:
+                nex  = nex.next
+
+    def kth_from_end(self, k: int):
+        if self.is_empty():
+            raise Exception("linkedlist is empty")
+            
+        if k > self.__size:
+            raise Exception("index out of range")
+
+        a = self.first
+        b = self.first
+        for i in range(k-1):
+            b = b.next
+        
+        while(True):
+            if b == self.last:
+                return a.value
+            
+            a = a.next
+            b = b.next
--- Data structures/linkedlist/main.py
+++ Data structures/linkedlist/main.py
@@ -1,14 +1,21 @@
 from LinkedList import LinkedList
 
 linked_list = LinkedList()
+# print(linked_list.to_array())
 linked_list.add_last(12)
+# print(linked_list.to_array())
 linked_list.add_last(13)
+# print(linked_list.to_array())
 linked_list.add_last(14)
+# print(linked_list.to_array())
 linked_list.add_last(15)
+# print(linked_list.to_array())
 linked_list.add_last(16)
+# print(linked_list.to_array())
 
-
-
+# print(linked_list.to_array())
+# linked_list.reverse()
+# print(linked_list.to_array())
 # print(linked_list.first)
 # print(linked_list.last)
-print(linked_list.contains(18))
+print(linked_list.kth_from_end(2))
