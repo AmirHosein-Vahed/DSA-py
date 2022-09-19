46e8067f41107cfdbc94a50a2bd350b773f441e6
AmirHosein-Vahed <vahed.ahv@gmail.com>
2022-09-19 07:32:59 +0430

    fibo tabular added

---
+++ dynamic programming/main.py
@@ -0,0 +1,28 @@
+from Fibonacci import Fibonacci
+
+# 1 -> 1
+# 2 -> 1
+# 3 -> 2
+# 4 -> 3
+# 5 -> 5
+# 6 -> 8
+# 7 -> 13
+fib =Fibonacci()
+
+# print(fib.fibo(4))
+# print(fib.fibo(5))
+# print(fib.fibo(6))
+# print(fib.fibo(7))
+# print(fib.fibo(50))      # Responding after a long time
+
+# print(fib.fibo_memo(4))
+# print(fib.fibo_memo(5))
+# print(fib.fibo_memo(6))
+# print(fib.fibo_memo(7))
+# print(fib.fibo_memo(50)) # Responding quickly
+
+# print(fib.fibo_tabu(4))
+# print(fib.fibo_tabu(5))
+# print(fib.fibo_tabu(6))
+# print(fib.fibo_tabu(7))
+# print(fib.fibo_tabu(50)) # Responding quickly
