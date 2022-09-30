75eedce6277a6e47227dce65d4c5057f938429e2
AmirHosein-Vahed <vahed.ahv@gmail.com>
2022-09-30 19:03:08 +0330

    tree updated with some new function such as is_BST, equals, height and printing...

--- Data structures/Tree/Tree.py
+++ Data structures/Tree/Tree.py
@@ -4,9 +4,7 @@ class Node:
         self.value = value
         self.right_child = None
         self.left_child = None
-        #        7
-        #    4       9
-        #  1  6    8  10
+
 
 class BSTree:
     def __init__(self):
@@ -47,6 +45,91 @@ class BSTree:
             
         return False
 
+    def print_nodes_at_distance(self, distance):
+        return self.__print_nodes_at_distance(distance, self.root)
+
+    def __print_nodes_at_distance(self, distance, root):
+        if root == None:
+            return
+            
+        if distance == 0:
+            print(root.value)
+            return
+        
+        self.__print_nodes_at_distance(distance-1, root.left_child)
+        self.__print_nodes_at_distance(distance-1, root.right_child)
+
+
+
+
+    def is_BST(self):
+        return self.__validate(self.root, float('-inf'), float('+inf'))
+
+    def __validate(self, root, minimum, maximum):
+        if root == None:
+            return True
+        
+        if not (minimum < root.value < maximum):
+            return False
+        
+        return self.__validate(root.left_child, minimum, root.value) and\
+               self.__validate(root.right_child, root.value, maximum)
+
+    
+    def equals(self, tree):
+        return self.__equals(self.root, tree.root)
+
+    def __equals(self, root1, root2):
+        if root1 == root2 == None:
+            return True
+        
+        if root1 != None and root2 != None:
+            return root1.value == root2.value and \
+                self.__equals(root1.left_child, root2.left_child) and \
+                self.__equals(root1.right_child, root2.right_child)
+        
+        return False
+
+    
+    def height(self):
+        return self.__height(self.root)
+
+    def __height(self, root):
+        if root == None:
+            return -1
+        # if root.left_child == root.right_child == None:
+        #     return 0
+        return 1 + max(self.__height(root.left_child), self.__height(root.right_child))
+
+
+    def min(self):
+        # for BST:
+        # current = self.root
+        # last    = current
+        # while(current != None):
+        #     last = current
+        #     current = current.left_child
+        # return last.value
+
+        return self.__min(self.root)
+
+    def __min(self, root):
+        if root.left_child == root.right_child == None:
+            return root.value
+        
+        elif root.left_child == None:
+            right = self.__min(root.right_child)
+            return min(right, root.value)
+
+        elif root.right_child == None:
+            left  = self.__min(root.left_child)
+            return min(left, root.value)
+
+        else:
+            left  = self.__min(root.left_child)
+            right = self.__min(root.right_child)
+            return min(left, right, root.value)
+
     
     def dfs_pre_order(self):
         self.__traverse_pre_order(self.root)
@@ -65,7 +148,7 @@ class BSTree:
         self.__traverse_pre_order(root.left_child)
         self.__traverse_pre_order(root.right_child)
  
- 
+
     def __traverse_in_order(self, root):
         if root == None:
             return
--- Data structures/Tree/main.py
+++ Data structures/Tree/main.py
@@ -1,5 +1,8 @@
-from Tree import BSTree
+from Tree import BSTree, Node
 
+#        7
+#    4       9
+#  1  6    8  10
 tree = BSTree()
 tree.insert(7)
 tree.insert(4)
@@ -9,11 +12,38 @@ tree.insert(6)
 tree.insert(8)
 tree.insert(10)
 
+tree.print_nodes_at_distance(2)
+
+#        7
+#    4       9
+#  1  6    8  10
+#   2
+# tree2 = BSTree()
+# tree2.insert(7)
+# tree2.insert(4)
+# tree2.insert(9)
+# tree2.insert(1)
+# tree2.insert(6)
+# tree2.insert(8)
+# tree2.insert(10)
+# tree2.insert(2)
+
+# tree3 = BSTree()
+# tree3.insert(10)
+# tree3.root.left_child = Node(9)
+# tree3.root.right_child = Node(12)
+# print(tree3.is_BST())
+
+# print(tree.equals(tree2))
+
+# print(tree.height())
+# print(tree.min())
+
 # print(tree.find(8))
 
 # tree.dfs_pre_order()
 # tree.dfs_in_order()
-tree.dfs_post_order()
+# tree.dfs_post_order()
 
 # print(tree.find(12))
 # print(tree.root.value)
